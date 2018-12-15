6b2261ef7d86aebdc9cbe7f617b7ae463eee31be
Roj <Roj@users.noreply.github.com>
2018-12-15 20:38:30 -0300

    Función auxiliar para correr un código como módulo.
    Esto permite separar el namespace del caller del código del callee.

--- worker/worker.py
+++ worker/worker.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 import ast
+import importlib
 
 class Worker(ABC):
     def __init__(self, ejercicios):
@@ -22,6 +23,19 @@ class Worker(ABC):
                 if nodo.id in metodos_prohibidos:
                     return False
         return True
+
+    @staticmethod
+    def correr_como_modulo(codigo, modulo = None):
+        """Escribe el código a un archivo y lo importa para poder utilizarlo.
+        Como funciona de módulo, el namespace es reservado y no puede acceder
+        a nuestras variables."""
+        with open("ejercicio.py","w") as f:
+            f.write(codigo)
+        # Si corremos de nuevo __import__, se usa el cache y no se refleja el
+        # nuevo código. importlib.reload arregla esto.
+        if modulo is not None:
+            return importlib.reload(modulo)
+        return importlib.__import__("ejercicio")
 
     @abstractmethod
     def correr(self):
