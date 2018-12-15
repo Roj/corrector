6d7c7f4a2239f78e1f887158cd459e10fd923733
joaquintz <joa.dev@live.com>
2018-12-15 08:16:20 +0100

    Clase corrector que tiene las guías.
    La idea de los YAML es que sea fácil agregar ejercicios. Cada ejercicio
    tiene un enunciado y un archivo de salida esperado, con el cual se
    contrastará la salida del código enviado.

---
+++ corrector.py
@@ -0,0 +1,21 @@
+import yaml
+import os
+import yaml
+
+class Corrector:
+    def __init__(self):
+        self.cargar_guias()
+
+    def cargar_guias(self):
+        archivos = os.listdir("guias/")
+        self.guias = []
+        for archivo in archivos:
+            if archivo[-4:] != ".yml":
+                continue
+            with open("guias/" + archivo) as f:
+                print("Cargando "+archivo)
+                guia = yaml.load(f)
+                self.guias.append(guia)
+
+    def nombres_guias(self):
+        return [guia["titulo"] for guia in self.guias]
