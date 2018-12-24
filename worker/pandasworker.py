63b95e2a2cc6436f8587163008c7b762107218a2
Roj <Roj@users.noreply.github.com>
2018-12-24 03:16:35 -0300

    Factorizo los resultados de PandasWorker.
    Tal vez debería ir directamente en la clase padre, lo voy a revisitar
    cuando esté SparkWorker.

--- worker/pandasworker.py
+++ worker/pandasworker.py
@@ -2,21 +2,26 @@ from worker import Worker
 import pandas as pd
 
 class PandasWorker(Worker):
+    def __init__(self, ejercicios):
+        super().__init__(ejercicios)
+        self.resultados = []
+
     def preparar_codigo(self, codigo):
         codigo = ["\t" + linea for linea in codigo.split("\n")]
         codigo = ["def programa(pd, datos):"] + codigo + ["\treturn datos"]
         return "\n".join(codigo)
 
+    def agregar_resultado(self, output="", error=""):
+        self.resultados.append({"output": output, "error": error})
+
     def correr(self):
-        self.resultados = []
         modulo = None
         for ejercicio in self.ejercicios:
             codigo = ejercicio["codigo"]
             if not Worker.codigo_es_seguro(codigo):
-                self.resultados.append({
-                    "error": "Código no seguro o con errores de sintaxis.",
-                    "output": ""
-                })
+                self.agregar_resultado(
+                    error = "Código no seguro o con errores de sintaxis."
+                )
                 continue
             # Correr el código del alumno.
             codigo = self.preparar_codigo(codigo)
@@ -25,15 +30,13 @@ class PandasWorker(Worker):
             datos = pd.read_csv("datos/"+ejercicio["archivo_datos"])
             try:
                 output = modulo.programa(pd, datos)
-                self.resultados.append({
-                    "error": "",
-                    "output": output.to_json()
-                })
+                self.agregar_resultado(
+                    output = output.to_json()
+                )
             except Exception as e:
-                self.resultados.append({
-                    "error": "Error en el código: " + str(e),
-                    "output": ""
-                })
+                self.agregar_resultado(
+                    error = "Error en el código: " + str(e)
+                )
 
     def respuestas(self):
         return self.resultados
