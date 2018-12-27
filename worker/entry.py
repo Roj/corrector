7ac569993b80fa4d31ab714aff8f7f96360da53f
Roj <Roj@users.noreply.github.com>
2018-12-27 01:02:42 -0300

    Esqueleto de servidor para Docker.
    Haciendo algunas pruebas encontré que inicializar un SparkContext es
    bastante lento (~5seg), esto agregado al overhead de levantar el
    contenedor de Docker es mucho.
    La idea ahora es que el contenedor de Docker al levantarse cargue las
    librerías y posiblemente los datos, para luego quedarse escuchando.
    De esta manera, lo único que hay que hacer al procesar un trabajo es
    correr el código con el ambiente cargado.
    
    El servidor subido en este commit funciona, acepta conexiones y espera
    que el cliente tenga el siguiente flujo:
    s = socket(...)
    s.connect(...)
    s.send(...); ... ; s.send(...)
    s.shutdown(1) # esto est SHUT_WR
    respuesta = s.recv(...)
    
    Es decir, la conexión es de una sola petición por parte del cliente y una
    sola respuesta por parte del servidor.
    
    Está mockeado el procesamiento del trabajo.

--- worker/entry.py
+++ worker/entry.py
@@ -1,19 +1,70 @@
 #!/usr/local/bin/python3
 import sys
-import argparse
 import json
+import socket
+import logging
 from pandasworker import PandasWorker
 
+PORT = 5007
+BUFFER_SIZE = 1024
+class TCPServer:
+    def __init__(self):
+        self.logger = logging.getLogger()
+        self.logger.addHandler(logging.StreamHandler())
+        self.logger.setLevel(logging.DEBUG)
+
+        self.logger.info("Iniciando servidor...")
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.bind((socket.gethostname(), PORT))
+        self.socket.listen(10)
+
+    def correr(self):
+        """Bucle principal del servidor. Acepta conexiones y hace el dispatch
+        en el mismo hilo."""
+        self.logger.info("Escuchando..")
+        while True:
+            (connection, address) = self.socket.accept()
+            self.logger.info("Conexión aceptada de {}".format(address))
+            self.procesar_cliente(connection)
+            connection.close()
+            self.logger.info("Conexión cerrada")
+
+    def procesar_cliente(self, connection):
+        """Maneja un socket a cliente, recibiendo toda la información hasta
+        que el cliente avise que no escribirá más con shutdown(WR).
+        Este esquema es similar al que utiliza HTTP.
+        Se procesa el trabajo y se le envía una respuesta.
+        Esta función abstrae al resto del JSON."""
+        buffer = connection.recv(BUFFER_SIZE)
+        request = b""
+        while buffer:
+            self.logger.debug("Buffer cargado de longitud: {}".format(buffer))
+            request += buffer
+            buffer = connection.recv(BUFFER_SIZE)
+
+        self.logger.debug("Datos recibidos: {}".format(request))
+        # Cargamos el trabajo y lo mandamos a procesar.
+        trabajo = json.loads(request)
+        respuesta = self.correr_trabajo(trabajo)
+        self.logger.debug("Enviando respuesta: {}".format(respuesta))
+        #sendall() nos abstrae del loop de envío
+        connection.sendall(json.dumps(respuesta).encode("utf-8"))
+
+    def correr_trabajo(self, trabajo):
+        return {"error": "", "output": "todo OK!"}
+
+    def __del__(self):
+        self.socket.close()
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("worker_type")
-    args = parser.parse_args()
-
+    # Preparamos el servidor
+    ServerWorker = TCPServer()
+    ServerWorker.correr()
     # Cargamos el json de la entrada estándar.
-    ejercicios = json.loads(sys.stdin.read())
+    #ejercicios = json.loads(sys.stdin.read())
 
-    if args.worker_type == "pandas":
-        worker = PandasWorker(ejercicios)
-
-    worker.correr()
-    print(json.dumps(worker.respuestas()))
+    #if args.worker_type == "pandas":
+    #    worker = PandasWorker(ejercicios)
+0
+    #worker.correr()
+    #print(json.dumps(worker.respuestas()))
